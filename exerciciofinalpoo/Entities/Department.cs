444b3ae203776ce14f8ef2780c17bf747e5b210d
Rodrigo Guedes <rodrigo.guedes.f@gmail.com>
2026-05-08 00:21:04 -0300

    Projeto inicial

---
+++ exerciciofinalpoo/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace exerciciofinalpoo.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ exerciciofinalpoo/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace exerciciofinalpoo.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
